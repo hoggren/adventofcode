0b1945c2c7e647116aaf5fcf38ad4dc851c545c0
Patrik Höggren <paho@impera.se>
2017-12-07 19:03:35 +0100

    5a

---
+++ 5a.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+(async () => {
+    let input = await readFile('./5a.bin', 'ascii');
+    let insts = input.split('\r\n').map(x => Number(x));
+
+    let i = 0;
+    let steps = 0;
+
+    while (true) {
+        steps++;
+
+        const jumps = insts[i];
+        insts[i] += 1;
+        i += jumps;
+
+        if (i >= insts.length)
+            break;
+    }
+
+    console.log(steps);
+})();
